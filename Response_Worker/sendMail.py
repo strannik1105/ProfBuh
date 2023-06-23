0b2e8f7d5d58763ee8b30b4f845cdd383aa59dea
danielkoreshkov@gmail.com <danielkoreshkov@gmail.com>
2023-06-24 00:11:59 +0700

    makeResponse: добавил отправку оповещения по почте

---
+++ Response_Worker/sendMail.py
@@ -0,0 +1,24 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+from settings import TOKEN, MAIL
+
+def createConnection():
+    connection = smtplib.SMTP_SSL('smtp.mail.ru: 465')
+    connection.login(MAIL, TOKEN)
+    return connection
+
+def sendMessage(mailTo, link):
+    server = createConnection()
+    msg = MIMEMultipart()
+    msg['From'] = MAIL
+    msg['To'] = mailTo
+    msg['Subject'] = "Твоя статья готова!"
+    message = f"Статья сформированна!\nСкачать ее можно по ссылке: {link}"
+    msg.attach(MIMEText(message, 'plain'))
+    server.sendmail(msg['From'], msg['To'], msg.as_string())
+    server.quit()
+    return True
+
+
+print(sendMessage("koreshkov200@mail.ru", "https://vk.com/kordan_09"))
---
+++ Response_Worker/sendVkMessage.py
@@ -0,0 +1,6 @@
+import vk_api
+from libs.rabbitmq_sender import send_message
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from token import token
+from translate import Translator
