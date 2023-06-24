eecc832c172531f830f3e407288c9ee0a23f4900
danielkoreshkov@gmail.com <danielkoreshkov@gmail.com>
2023-06-24 14:34:51 +0700

    начата работа над VK/TG bots

--- Response_Worker/sendVkMessage.py
+++ Response_Worker/sendVkMessage.py
@@ -1,6 +1,13 @@
 import vk_api
-from libs.rabbitmq_sender import send_message
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
-from vk_api.keyboard import VkKeyboard, VkKeyboardColor
-from token import token
-from translate import Translator
+from config import VK_TOKEN
+
+vk_bot = vk_api.VkApi(token=VK_TOKEN)
+session_bot = vk_bot.get_api()
+longpoll = VkBotLongPoll(vk_bot, 214053144)
+
+
+for event in longpoll.listen():
+    if event.type == VkBotEventType.MESSAGE_NEW:
+        peer = event.object.from_id
+        user_id = event.object.message["from_id"]
